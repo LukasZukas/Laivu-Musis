f459727cc50c8311f1e667c058ecfcb8f8371648
Mindaugas Matulaitis <minmat4@ktu.lt>
2025-03-10 20:49:35 +0200

    Add project files.

---
+++ WarshipBattle/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WarshipBattle.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult GameModes()
+        {
+            return View();
+        }
+        public IActionResult PlayAgainstBot()
+        {
+            return View();
+        }
+
+        public IActionResult PlayAgainstPlayer()
+        {
+            return View();
+        }
+
+    }
+}
