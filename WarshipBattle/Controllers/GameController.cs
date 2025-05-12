3352ac3d2cbfc36b2c43db687fe357ccfac37976
ArmArl <92205556+Cepsbi007@users.noreply.github.com>
2025-05-12 20:46:18 +0300

    Update GameController.cs

--- WarshipBattle/Controllers/GameController.cs
+++ WarshipBattle/Controllers/GameController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WarshipBattle.Areas.Identity.Pages.Account.Manage;
@@ -10,14 +10,6 @@ namespace WarshipBattle.Controllers
 {
     public class GameController : Controller
     {
-        public IActionResult GameModes()
-        {
-            return View();
-        }
-        public IActionResult PlayAgainstBot()
-        {
-            return View();
-        }
         private readonly UserManager<IdentityUser> _userManager;
         private readonly ApplicationDbContext _context;
 
@@ -25,6 +17,16 @@ namespace WarshipBattle.Controllers
         {
             _userManager = userManager;
             _context = context;
+        }
+
+        public IActionResult GameModes()
+        {
+            return View();
+        }
+
+        public IActionResult PlayAgainstBot()
+        {
+            return View();
         }
 
         public async Task<IActionResult> PlayAgainstPlayer()
@@ -40,6 +42,19 @@ namespace WarshipBattle.Controllers
                 return RedirectToAction("Index", "Home");
             }
 
+            var playerStats = await _context.PlayerStats.FirstOrDefaultAsync(ps => ps.UserId == userId);
+            if (playerStats == null)
+            {
+                playerStats = new PlayerStats
+                {
+                    UserId = userId,
+                    Victories = 0,
+                    LastUpdated = DateTime.UtcNow
+                };
+                _context.PlayerStats.Add(playerStats);
+                await _context.SaveChangesAsync();
+            }
+
             var friends = await _context.Friendships
                 .Where(f => f.User1Id == userId || f.User2Id == userId)
                 .Select(f => f.User1Id == userId ? f.User2 : f.User1)
@@ -52,6 +67,7 @@ namespace WarshipBattle.Controllers
 
             return View(model);
         }
+
         public async Task<IActionResult> PrivateRoom(int roomId)
         {
             var userId = _userManager.GetUserId(User);
@@ -65,6 +81,80 @@ namespace WarshipBattle.Controllers
             return View("PrivateRoom", roomId);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> RecordVictory(int roomId)
+        {
+            if (User?.Identity == null || !User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
 
+            var userId = _userManager.GetUserId(User);
+            if (string.IsNullOrEmpty(userId))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var invitation = await _context.GameInvitations.FindAsync(roomId);
+            if (invitation == null || (invitation.SenderId != userId && invitation.ReceiverId != userId))
+            {
+                return RedirectToAction("PlayAgainstPlayer");
+            }
+
+            var playerStats = await _context.PlayerStats.FirstOrDefaultAsync(ps => ps.UserId == userId);
+            if (playerStats == null)
+            {
+                playerStats = new PlayerStats
+                {
+                    UserId = userId,
+                    Victories = 1,
+                    LastUpdated = DateTime.UtcNow
+                };
+                _context.PlayerStats.Add(playerStats);
+            }
+            else
+            {
+                playerStats.Victories += 1;
+                playerStats.LastUpdated = DateTime.UtcNow;
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction("PlayAgainstPlayer");
+        }
+
+        public async Task<IActionResult> LeaderBoard()
+        {
+            var allUsers = await _userManager.Users.ToListAsync();
+
+            foreach (var user in allUsers)
+            {
+                var playerStats = await _context.PlayerStats.FirstOrDefaultAsync(ps => ps.UserId == user.Id);
+                if (playerStats == null)
+                {
+                    playerStats = new PlayerStats
+                    {
+                        UserId = user.Id,
+                        Victories = 0,
+                        LastUpdated = DateTime.UtcNow
+                    };
+                    _context.PlayerStats.Add(playerStats);
+                }
+            }
+            await _context.SaveChangesAsync();
+
+            var topPlayers = await _context.PlayerStats
+                .OrderByDescending(ps => ps.Victories)
+                .Take(10)
+                .ToListAsync();
+
+            var leaderboardData = new List<(string Username, int Victories)>();
+            foreach (var stats in topPlayers)
+            {
+                var user = await _userManager.FindByIdAsync(stats.UserId);
+                leaderboardData.Add((user?.UserName ?? stats.UserId, stats.Victories));
+            }
+
+            return View(leaderboardData);
+        }
     }
 }
